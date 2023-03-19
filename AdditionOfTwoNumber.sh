cb6798261afb3ccef3123754a31844d607cfa282
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-03-19 16:31:08 +0530

    first commit

---
+++ AdditionOfTwoNumber.sh
@@ -0,0 +1,6 @@
+read -p "Enter 1st Number: " x
+read -p "Enter 2nd Number: " y
+echo $x
+echo $y
+z= $(($x + $y))
+echo $z
---
+++ GreaterNumber.sh
@@ -0,0 +1,6 @@
+read -p "Enter the number: " a
+read -p "Enter the number: " b
+if [ $a -gt $b ]
+then
+echo $a is greater than $b
+fi
